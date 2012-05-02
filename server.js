d0a2438b498616eaa76cc97609a7989711809dad
Peter Downs <peter.l.downs@gmail.com>
2012-05-02 15:01:52 -0400

    Fixes syntax error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('http').createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type:" : "text/plain"});
 	output = "Hello World!\n";
 	for (k in request.headers) {
-		output += k + "=" = request.headers[k] + '\n';
+		output += (k + "=" + request.headers[k] + '\n');
 	}
 	response.end(output);
 }).listen(8080);
