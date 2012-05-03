36df53d06cd5ebbfb96b02a649fa23ada60c4bce
Peter Downs <peter.l.downs@gmail.com>
2012-05-03 16:49:44 -0400

    Removes default permissions that were causing errors

--- conf.js
+++ conf.js
@@ -1,8 +1,8 @@
 module.exports = {
   appId: '356680201056562',
   appSecret: '867364fbfb78b541e3411fb5b9576724',
+  secret: 'shhh, don\'t tell anyone!!',
   perms: [
-    'id', 'name', 'locale', 'friends',
     'friends_location',
     'friends_hometown',
     'friends_status'
