503b386a2f6c2579eec25a15dffba780b55fba18
Peter Downs <peter.l.downs@gmail.com>
2012-05-03 15:50:28 -0400

    Changes middleware ordering, fixes spacing

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-
 /**
  * Module dependencies.
  */
-
 var express = require('express');
-
 var app = module.exports = express.createServer();
 
 // Configuration
@@ -14,9 +11,9 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
