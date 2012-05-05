b9c8b46c214fd7afc29524f422220cbdfa0493b7
Peter Downs <peter.l.downs@gmail.com>
2012-05-05 15:00:54 -0400

    Adds number of friends indicator

--- public/js/index.js
+++ public/js/index.js
@@ -10,14 +10,12 @@ $(document).ready(function(){
   // Helper functions
   
   var isNearby = function(a, b){
-    console.log("Checking to see if '%s' is nearby '%s'", a, b);
+    console.log("Checking to see if '%s' is a substring of '%s'", b, a);
     return (a.toLowerCase().indexOf(b.toLowerCase()) !== -1);
   }
 
   var makePerson = function(friend){
-    console.log(friend);
     var id = friend.username || friend.id;
-    console.log("id = %s", id);
     var res = '<li>' +
                 '<a href="https://facebook.com/'+id+'">' +
                   '<div class="person">' +
@@ -41,6 +39,7 @@ $(document).ready(function(){
     var me = data;
     FB('/me/friends', function(data){
       var friends = data.data;
+      $('.loading').prepend("<p>Searching your "+friends.length+" friends...");
       console.log("Looking through "+friends.length+" friends");
       
       var people = $('ul#people');
@@ -63,17 +62,15 @@ $(document).ready(function(){
           else{
             console.log("Trying to grab %s's location info", friend_info.name);
             FB('/'+id+'/locations', function(loc_info){
-              console.log("LOCATION INFO:");
-              console.log(loc_info);
               loc_info.data.some(function(_event){
                 friend_info.from = _event.from;
                 var event_loc = _event.place;
                 if (!event_loc){
+                  console.log("No location info for %s", friend_info.name);
                   return false;
                 }
                 var loc_string = [event_loc.name, event_loc.location.city, event_loc.location.state, event_loc.location.country].join(', ');
                 loc_string = String(loc_string);
-                console.log(loc_string);
                 if(isNearby(loc_string, place)){
                   console.log('%s is nearby %s!', friend_info.location.name, loc_string);
                   addPersonTo('ul#people', friend_info);
